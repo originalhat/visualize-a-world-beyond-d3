c067f0b34f51733209eead04f273def7312913f9
Devin Brown <devin.g.brown@gmail.com>
2019-01-05 06:38:54 -0700

    making your own chart is pretty easy

--- src/App.js
+++ src/App.js
@@ -1,27 +1,40 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
   render() {
+    let WIDTH = 500;
+    let HEIGHT = 300;
+    let TICK_COUNT = 5;
+    let MAX_X = Math.max(...this.props.data.map(d => d.x));
+    let MAX_Y = Math.max(...this.props.data.map(d => d.y));
+
+    let x = val => val / MAX_X * WIDTH;
+    let y = val => HEIGHT - val / MAX_Y * HEIGHT;
+    let x_ticks = this._getTicks(TICK_COUNT, MAX_X);
+    let y_ticks = this._getTicks(TICK_COUNT, MAX_Y).reverse();
+
+    let d = `M${x(this.props.data[0].x)} ${y(this.props.data[0].y)} ${this.props.data.slice(1).map(d => {
+      return `L${x(d.x)} ${y(d.y)}`;
+    }).join(' ')}`;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="LineChart" style={{width: `${WIDTH}px`, height: `${HEIGHT}px`}}>
+        <svg width={WIDTH} height={HEIGHT}>
+          <path d={d}/>
+        </svg>
+        <div className="x-axis">
+          {x_ticks.map(v => <div data-value={v}/>)}
+        </div>
+        <div className="y-axis">
+          {y_ticks.map(v => <div data-value={v}/>)}
+        </div>
       </div>
     );
+  }
+
+  _getTicks(count, max) {
+    return [...Array(count).keys()].map(d => max / (count - 1) * parseInt(d))
   }
 }
 
